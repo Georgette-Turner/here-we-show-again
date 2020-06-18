80671b65c0834a1b5a6eeb7b1c7dced7e843d2c3
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-06-18 14:34:27 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const mainElement = document.querySelector('.katas-list')
+
+// /KATA 1
+
+// parent element
+const kata1Heading = document.creteElement('h3')
+parentElement.append('KATA 1')
+parentElement.append(kata1Heading)
+
+
+// appens our Kata 1 reults to this page
+
+// KATA 2
